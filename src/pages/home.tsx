21c479c157171e8fe0e51a6dfdabbcbf22738b76
zzj <1005978400@qq.com>
2024-01-09 10:42:52 +0800

    添加header

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,12 @@
-import React from "react";
-import './home.scss'
+import "./home.scss";
+import headerBg from "../images/header.png";
 
 export const Home = () => {
-    return (
-        <div>
-            你好
-        </div>
-    )
-}
+  return (
+    <div>
+      <div className="home">
+        <header style={{ backgroundImage: `url(${headerBg})` }}></header>
+      </div>
+    </div>
+  );
+};
